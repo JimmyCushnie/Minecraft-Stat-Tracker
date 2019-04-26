07cc76315d53e085d7869865b8ff9352cab4fffa
Jimmy Cushnie <hello@jimmycushnie.com>
2019-04-26 01:05:59 -0400

    user Pluralizer results in CommandFormatter

---
+++ tools/CommandFormatter/CommandFormatter/Word.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CommandFormatter
+{
+    class Word
+    {
+        public string single;
+        public string plural;
+
+        public static implicit operator Word((string, string) tuple)
+            => new Word()
+            {
+                single = tuple.Item1,
+                plural = tuple.Item2,
+            };
+    }
+}
